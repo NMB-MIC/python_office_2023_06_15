f525239f265a42b11653c26dcbb741d52e2527f3
suraphop <mic@admin.com>
2023-06-15 12:54:38 +0700

    update

---
+++ test2.py
@@ -0,0 +1,31 @@
+# %% [markdown]
+# test run jupyter notebook
+
+# %%
+print("test")
+
+# %%
+print("test2")
+
+# %% [markdown]
+# # Hello world
+
+# %% [markdown]
+# ## Hello world2
+
+# %% [markdown]
+# ### Hello world3
+
+# %% [markdown]
+# #### Hello world4
+
+# %% [markdown]
+# Hello world5
+
+# %% [markdown]
+# - bullet1
+# - bullet2
+#   - bullet3
+#     - bullet4
+
+
